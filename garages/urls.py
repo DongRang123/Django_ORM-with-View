ec5a445a363d1fc55a569fa81ad7245a828c11d0
kimjingu <jso732@naver.com>
2025-03-28 17:34:51 +0900

    first

--- garages/urls.py
+++ garages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+app_name ='garages'
 
 urlpatterns = [
-    
+    path('new/',views.new,name='new'),
+    path('',views.index,name='index'),
+    path('/create',views.create,name='create')
 ]
